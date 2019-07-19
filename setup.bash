e077a3accc6ad426387ed1159a5341724aaeb4a5
shouta0812 <shouta3012@ezweb.ne.jp>
2019-07-19 03:19:39 +0000

    Change the setup way
    Since the setup way with cron is not standard, Makefile is prepared.
    Moreover, the setup script is script is placed to /etc/init.d.

--- setup.bash
+++ setup.bash
@@ -1,5 +1,12 @@
 #!/bin/bash -xve
-
+# Start/stop the raspimouse.
+#
+### BEGIN INIT INFO 
+# Provides:          raspimouse 
+# Default-Start:     2 3 4 5 
+# Default-Stop:      0 1 6
+# Description:       mount my backupdisc.
+### END INIT INFO 
 exec 2> /tmp/setup.log
 
 cd /home/ubuntu/RaspberryPiMouse/src/drivers/
